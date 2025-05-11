cfd880e5d649b2267381f8d824f8eb327f90a943
xennin <151030338+xennin@users.noreply.github.com>
2025-05-11 12:05:41 +0800

    Update maintainers name

--- arrow_fog.mk
+++ arrow_fog.mk
@@ -12,8 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/arrow/config/common.mk)
 
 # ArrowOS Additional Flags
-DEVICE_MAINTAINER := Iverz
-DEVICE_MAINTAINER_LINK := https://t.me/JustVerz
+DEVICE_MAINTAINER := Emanuel
+DEVICE_MAINTAINER_LINK := 
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := false
